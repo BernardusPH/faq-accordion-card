7f89cd8768fd9ca0892cf7a9af42d50e4575a6ac
BernardusPH <bernardh0602@gmail.com>
2023-02-05 15:32:17 +0200

    fixed button

--- src/components/main/text/Question.jsx
+++ src/components/main/text/Question.jsx
@@ -15,8 +15,8 @@ function questionClickHandle(){
     return (
         <div className="question-container" >
             <div className="question-answer-container">
-                <button id={props.id} className="question-text" onClick={questionClickHandle}>{props.question}</button>
-                <button id={props.id + "arrow"} className="arrow" onClick={questionClickHandle} style={showAns?{transform: "rotate(180deg)"}:{}}> <svg width="10" height="7" xmlns="http://www.w3.org/2000/svg"><path d="M1 .799l4 4 4-4" stroke="#F47B56" strokeWidth="2" fill="none" fillRule="evenodd" /></svg> </button>
+                <button id={props.id} aria-label={props.id} className="question-text" onClick={questionClickHandle}>{props.question}</button>
+                <button aria-label={props.id + "arrow"} className="arrow" onClick={questionClickHandle} style={showAns?{transform: "rotate(180deg)"}:{}}> <svg width="10" height="7" xmlns="http://www.w3.org/2000/svg"><path d="M1 .799l4 4 4-4" stroke="#F47B56" strokeWidth="2" fill="none" fillRule="evenodd" /></svg> </button>
             </div>
 
             <p className= "answer" style={showAns?{display:"block"}:{display:"none"}} >{props.answer}</p>
